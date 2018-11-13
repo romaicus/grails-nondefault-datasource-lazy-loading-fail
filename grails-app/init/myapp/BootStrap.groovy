2ee15ec958084e492b40ee891fa465e643d88c11
asimm <asimm@buttonwood.com.au>
2018-11-14 10:13:02 +1100

    logging properties

--- grails-app/init/myapp/BootStrap.groovy
+++ grails-app/init/myapp/BootStrap.groovy
@@ -1,12 +1,24 @@
 package myapp
 
+import grails.core.GrailsApplication
+
 class BootStrap {
 
     MyService myService
+    def grailsApplication
 
     def init = { servletContext ->
         myService.init()
         myService.verify()
+
+
+        def properties = grailsApplication.config.toProperties()
+
+        log.info("Properties:")
+        properties.propertyNames().each { String pName ->
+            log.info(pName + " -> " + properties.getProperty(pName))
+        }
+
     }
     def destroy = {
     }
